7e8cfe3a9c5eca475f2c607a7c8b53e7e2d55262
FISH2500 <kita.shou2006@gmail.com>
2025-06-11 09:39:49 +0900

    player2script

---
+++ Assets/player2 Script.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class player2Script : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("player2desu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
